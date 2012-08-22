76e4be7058143b5b0d39f3b1e6ec45d37819ebd4
fhoepelman <hoepelman03@hotmail.com>
2012-08-21 22:25:12 -0700

    Trabajar Con el DataAccess,creando el metodo de InsertClientes

---
+++ SGPP/DataAccess/ClientesDA.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+
+
+namespace DataAccess
+{
+    class ClientesDA
+    {
+
+
+        public void InsertClientes(string CodCliente, string Nombre)
+        {
+
+            SqlCommand ObjCmd = new SqlCommand("Sp_InsertClientes", connection.Get);
+            ObjCmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter PrmCodCliente = new SqlParameter("@CodCliente", CodCliente);
+            PrmCodCliente.DbType = DbType.String;
+            SqlParameter PrmNombre = new SqlParameter("@Nombre", Nombre);
+            PrmNombre.DbType = DbType.String;
+
+
+            
+            
+        
+
+
+        
+        }
+
+        public void UpdateClientes()
+        {
+
+
+        }
+
+
+        public void DeleteClientes()
+        {
+
+
+        }
+
+        public void SearchClientes()
+        {
+
+
+        }
+
+
+    }
+}
